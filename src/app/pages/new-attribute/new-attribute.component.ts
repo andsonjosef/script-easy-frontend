b84e307e898b95047f235e0a377751985a1ce5b9
andsonjosef <andson.josef@gmail.com>
2018-10-15 10:52:18 -0300

    new attributes rows

--- src/app/pages/new-attribute/new-attribute.component.ts
+++ src/app/pages/new-attribute/new-attribute.component.ts
@@ -36,6 +36,12 @@ export class NewAttributeComponent implements OnInit {
       }
     }
   };
+  
+  lindexs = ["", "PRIMARY", "FOREIGN", "UNIQUE"];
+  dtypes = ["INT", "VARCHAR", "BOOLEAN", "DATE", "TEXT"];
+  ntypes = ["TINYINT", "SMALLINT", "MEDIUMINT", "INT", "BIGINT", "DECIMAL", "FLOAT", "DOUBLE", "REAL", "BIT", "BOOLEAN", "SERIAL", "NUMERIC", "RAW"];
+  ttypes = ["DATE", "DATETIME", "TIMESTAMP", "TIME", "YEAR"];
+  stypes = ["CHAR", "VARCHAR", "TINYTEXT", "TEXT", "MEDIUMTEXT", "LONGTEXT", "BINARY", "VARBINARY", "TINYBLOB", "MEDIUMBLOB", "BLOB", "LONGBLOB", "ENUM", "SET", "CLOB"];
   names: string[] = [];
   ais: boolean[] = [];
   defaultAs: string[] = [];
@@ -45,10 +51,11 @@ export class NewAttributeComponent implements OnInit {
   types: string[] = [];
   comments: string[] = [];
   referencesTables: string[] = [];
-  countries = ['USA', 'Canada', 'Uk'];
   attributes: AttributeDTO[] = [];
-  rows: number[] = [];
+  srows: boolean[] = [];
+  rows: boolean[] = [];
   id: number;
+  add: number = 1;
   constructor(private router: Router,
     public tableService: TableService,
     public attributeService: AttributeService,
@@ -61,21 +68,96 @@ export class NewAttributeComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.rows = [0,1,2];
+    this.rows = [false, false, false, false];
   }
 
+  addRow(add: number) {
+    let i = 0;
 
+
+    while (i < add) {
+      this.rows.push(false);
+
+
+      i++;
+    }
+  }
+
+  removeRow() {
+
+    let i = 0;
+    console.log("lenght" + this.srows.length);
+    while (i < this.srows.length) {
+      console.log("ture? " + this.srows[i] + " i " + i);
+      if (this.srows[i] == true) {
+        this.names.splice(i, 1);
+        this.ais.splice(i, 1);
+        this.defaultAs.splice(i, 1);
+        this.indexs.splice(i, 1);
+        this.nullAs.splice(i, 1);
+        this.sizes.splice(i, 1);
+        this.types.splice(i, 1);
+        this.comments.splice(i, 1);
+        this.referencesTables.splice(i, 1);
+        this.srows.splice(i, 1);
+        this.rows.splice(i, 1);
+        console.log("lenght after" + this.srows.length);
+      }
+
+      i++;
+    }
+  }
+
+  test(){
+    let i = 0;
+    while(i < this.attributes.length){
+      this.attribute = this.attributes[i];
+      this.id = parseInt(this.router.url.substring(this.router.url.indexOf("/tables/") + 8, this.router.url.indexOf("/newAttribute")));
+      this.tableService.find(this.id)
+        .subscribe(response => {
+          this.attribute.table = response;
+
+        },
+          error => { this.toastr.error(this.message.getter()); });
+
+
+      this.attributeService.insert(this.attribute)
+        .subscribe(response => {
+
+          this.toastr.success('Attribute created!', 'Success!');
+        },
+          error => { this.toastr.error(this.message.getter()); });
+
+      console.log(this.attribute);
+      i++;
+    }
+  }
 
   new() {
     let i;
     i = 0;
 
-    while (i < this.names.length) {
+    while (i < this.rows.length) {
       if (this.ais[i] != true) {
         this.ais[i] = false;
       }
       if (this.nullAs[i] != true) {
         this.nullAs[i] = false;
+      }
+
+      if(this.names[i] == null || this.names[i] == ""){
+        this.names.splice(i, 1);
+        this.ais.splice(i, 1);
+        this.defaultAs.splice(i, 1);
+        this.indexs.splice(i, 1);
+        this.nullAs.splice(i, 1);
+        this.sizes.splice(i, 1);
+        this.types.splice(i, 1);
+        this.comments.splice(i, 1);
+        this.referencesTables.splice(i, 1);
+        this.srows.splice(i, 1);
+        this.rows.splice(i, 1);
+
       }
       this.attribute.name = this.names[i];
 
@@ -90,27 +172,22 @@ export class NewAttributeComponent implements OnInit {
       this.attributes[i] = this.attribute;
       this.id = parseInt(this.router.url.substring(this.router.url.indexOf("/tables/") + 8, this.router.url.indexOf("/newAttribute")));
       this.tableService.find(this.id)
-      .subscribe(response => {
-        this.attribute.table = response;
-       
-      },
-        error => { this.toastr.error(this.message.getter()); });
+        .subscribe(response => {
+          this.attribute.table = response;
 
-      
-        this.attributeService.insert(this.attribute)
-      .subscribe(response => {
-       
-        this.toastr.success('Table created!', 'Success!');
-      },
-        error => { this.toastr.error(this.message.getter()); });
+        },
+          error => { this.toastr.error(this.message.getter()); });
+
+
+      this.attributeService.insert(this.attribute)
+        .subscribe(response => {
+
+          this.toastr.success('Table created!', 'Success!');
+        },
+          error => { this.toastr.error(this.message.getter()); });
 
       console.log(this.attribute);
       i++;
     }
-
-
-
-
-
   }
 }
